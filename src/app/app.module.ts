f1063767e349ff2bc584f63003151c2f190e48f3
Julio Tran <julio.tran@gmail.com>
2022-04-02 01:50:09 -0300

    header y acerca de component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { BannerComponent } from './componets/banner/banner.component';
+import { AcercadeComponent } from './componets/acercade/acercade.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
+import { EducacionComponent } from './componets/educacion/educacion.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+  
+
   ],
   imports: [
     BrowserModule,
